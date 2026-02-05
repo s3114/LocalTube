390eac5bce9a667ff8b24c3ead5b4c24f01ae6da
LAPTOP-F033PNHO\soman <somamenishi.3114@gmail.com>
2026-02-05 19:40:51 +0900

    ;;

--- バックグラウンドで起動.ps1
+++
@@ -1,8 +0,0 @@
-$OutputEncoding = [System.Text.Encoding]::UTF8
-
-$baseDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-Set-Location $baseDir
-
-Start-Process "cmd.exe" `
-    -ArgumentList "/k call `"$baseDir\起動.bat`"" `
-    -WindowStyle Hidden
